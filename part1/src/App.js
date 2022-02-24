94edf26def3a1996ff27d6d5d3aa81509b0f98a1
sjeffry-o <kronizzyd@gmail.com>
2022-02-24 10:56:12 +0500

    step1 done; goal is to refactor code

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,53 @@
-const Info = (data) => {
-	console.log("info logging")
+const Header = (data) => {
 	return (
 	<div>
-	 <p>Hello everybody!</p>
-	 <p>I'm {data.name}</p>
+      		<h1>{data.course}</h1>
+	</div>
+	)
+}
+
+const Content = (data) => {
+	return (
+	<div>
+	      <p>
+		{data.part1} {data.exercises1}
+	      </p>
+	      <p>
+		{data.part2} {data.exercises2}
+	      </p>
+	      <p>
+		{data.part3} {data.exercises3}
+	      </p>
+	</div>
+	)
+}
+
+const Total = (data) => {
+	return (
+	<div>
+      	      <p>Number of exercises {data.exercises1 + data.exercises2 + data.exercises3}</p>
 	</div>
 	)
 }
 
 const App = () => {
-	console.log("and here they comes")
-	const now = new Date()
-	const funny_str = "☕☕"
-	const num = 10
-	return (
-	<div>
-	 <p>Hello EVERYONE!</p>
-	 <p>It is {now.toString()} and i bring you coffee {funny_str}</p>
-	 <p>Also we have a number {funny_str} {num} {funny_str}</p>
-	 <Info name="Moto Moto Hashimoto" />
-	</div>
-	)
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} 
+	       part2={part2} exercises2={exercises2} 
+ 	       part3={part3} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
 }
 
-export default App;
+export default App
