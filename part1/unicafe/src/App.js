264b171c5f0d17bccbd24d050439decac6e983f7
sjeffry-o <kronizzyd@gmail.com>
2022-02-26 12:28:31 +0500

    part1 step3; refactoring; Statistics component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -28,6 +28,19 @@ const PositivesPerc_Stat = ( {good, neutral, bad} ) => {
 	return (good / All_Stat({good, neutral, bad}) * 100)
 }
 
+const Statistics = ( {good, neutral, bad} ) => {
+	return (
+	<div>
+		<DisplayStat text="good" value={good} />
+		<DisplayStat text="neutral" value={neutral} />
+		<DisplayStat text="bad" value={bad} />
+		<DisplayStat text="All" value={All_Stat({good, neutral, bad})} />
+		<DisplayStat text="average" value={AVG_Stat({good, neutral, bad})} />
+		<DisplayStat text="positive" value={PositivesPerc_Stat({good, neutral, bad}) + " %"} />
+	</div>
+	)
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -44,12 +57,7 @@ const App = () => {
 	<Button onclick={() => setNeutral(neutral + 1)} text={"neutral"} />
 	<Button onclick={() => setBad(bad + 1)} text={"bad"} />
 	<Header text="statistics" />
-	<DisplayStat text="good" value={good} />
-	<DisplayStat text="neutral" value={neutral} />
-	<DisplayStat text="bad" value={bad} />
-	<DisplayStat text="All" value={All_Stat({good, neutral, bad})} />
-	<DisplayStat text="average" value={AVG_Stat({good, neutral, bad})} />
-	<DisplayStat text="positive" value={PositivesPerc_Stat({good, neutral, bad}) + " %"} />
+	<Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
