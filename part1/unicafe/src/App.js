db489db0b3711c1105169cb2cd890e51ef23e21a
sjeffry-o <kronizzyd@gmail.com>
2022-02-26 11:06:11 +0500

    part1 step1; primitive feedback collector and display

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = (props) => {
+	return (
+	<h1>{props.text}</h1>
+	)
 }
 
-export default App;
+const Button = (props) => {
+	return (
+	<button onClick={props.onclick}>{props.text}</button>
+	)
+}
+
+const DisplayStat = (props) => {
+	return (
+	<p>{props.text} {props.value}</p>
+	)
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+	<Header text="give feedback" />
+	<Button onclick={() => setGood(good + 1)} text={"good"} />
+	<Button onclick={() => setNeutral(neutral + 1)} text={"neutral"} />
+	<Button onclick={() => setBad(bad + 1)} text={"bad"} />
+	<Header text="statistics" />
+	<DisplayStat text="good" value={good} />
+	<DisplayStat text="neutral" value={neutral} />
+	<DisplayStat text="bad" value={bad} />
+    </div>
+  )
+}
+
+export default App
