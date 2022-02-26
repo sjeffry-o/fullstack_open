2adb6f2097722466d56ed19acc42a85a3fdd91f4
sjeffry-o <kronizzyd@gmail.com>
2022-02-26 12:35:55 +0500

    part1 step4,5; conditional rendering of stats; turns out step5 refactoring i did in the beginning :)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -29,17 +29,22 @@ const PositivesPerc_Stat = ( {good, neutral, bad} ) => {
 }
 
 const Statistics = ( {good, neutral, bad} ) => {
-	return (
-	<div>
-		<DisplayStat text="good" value={good} />
-		<DisplayStat text="neutral" value={neutral} />
-		<DisplayStat text="bad" value={bad} />
-		<DisplayStat text="All" value={All_Stat({good, neutral, bad})} />
-		<DisplayStat text="average" value={AVG_Stat({good, neutral, bad})} />
-		<DisplayStat text="positive" value={PositivesPerc_Stat({good, neutral, bad}) + " %"} />
-	</div>
-	)
+	if (All_Stat({ good,neutral,bad} ) === 0)
+		return (<p>No feedback given</p>)
+	else
+		return (
+		<div>
+			<DisplayStat text="good" value={good} />
+			<DisplayStat text="neutral" value={neutral} />
+			<DisplayStat text="bad" value={bad} />
+			<DisplayStat text="All" value={All_Stat({good, neutral, bad})} />
+			<DisplayStat text="average" value={AVG_Stat({good, neutral, bad})} />
+			<DisplayStat text="positive" value={PositivesPerc_Stat({good, neutral, bad}) + " %"} />
+		</div>
+		)
 }
+
+
 
 const App = () => {
   // save clicks of each button to its own state
