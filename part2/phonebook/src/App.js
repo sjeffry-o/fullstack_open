d275f3bfa607964292a2549e8fb8a02d618e16e0
sjeffry-o <kronizzyd@gmail.com>
2022-03-01 10:11:04 +0500

    phonebook step1; form setter, button submit w/ concat

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+	  event.preventDefault()
+
+	  const new_person = {name: newName}
+	  setPersons(persons.concat(new_person))
+	  setNewName('')
+  }
+
+  const nameSetting = (event) => {
+	  setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={nameSetting}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
