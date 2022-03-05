6a5019d4b1f859f8bc00cd6d6ba8cd9b69f3131a
sjeffry-o <kronizzyd@gmail.com>
2022-03-05 16:03:17 +0500

    phonebook backend step3; person retrieving by id, handle 404

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -13,8 +13,31 @@ const read_parseJson = (filepath) => {
 	return persons
 }
 
+const get_person = (id) => {
+	const db = read_parseJson('db.json')
+	const person = db.filter(person => person.id === id)[0]
+	console.log(person)
+	return person
+}
+
 app.get('/api/persons', (request, response) => {
 	response.json(read_parseJson('db.json'))
+})
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const person = get_person(id)
+	if (person)
+		response.json(person)
+	else
+		response.status(404).end()
+})
+
+app.get('/info', (request, response) => {
+	parsed_db = read_parseJson('db.json')
+	response.send(`
+		<p>Phonebook has info for ${parsed_db.length} people</p>
+		<p>${new Date().toISOString()}</p>`)
 })
 
 const PORT = 3001
