4602ac1e6266080d674823c045f28d637461cb41
sjeffry-o <kronizzyd@gmail.com>
2022-03-06 14:20:27 +0500

    phonebook backend step7,8; morgan middleware custom logging of requests

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,34 @@
+const morgan = require('morgan')
 const express = require('express')
 const fs = require('fs');
 const path = require('path');
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan((tokens, req, res) => {
+  const method = tokens.method(req, res)
+  //console.log(method, typeof method)
+  //console.log(req.body)
+  if (method === 'POST')
+	  return [
+	    tokens.method(req, res),
+	    tokens.url(req, res),
+	    tokens.status(req, res),
+	    tokens.res(req, res, 'content-length'), '-',
+	    tokens['response-time'](req, res), 'ms',
+	    JSON.stringify(req.body)
+	  ].join(' ')
+  else
+	  return [
+	    tokens.method(req, res),
+	    tokens.url(req, res),
+	    tokens.status(req, res),
+	    tokens.res(req, res, 'content-length'), '-',
+	    tokens['response-time'](req, res), 'ms'
+	  ].join(' ')
+}))
+//app.use(morgan('tiny'))
 const http = require('http')
 
 const getRandomInt = (max) => {
