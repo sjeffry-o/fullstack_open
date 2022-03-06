847615d5c799fd75f683689915dfb5465541226c
sjeffry-o <kronizzyd@gmail.com>
2022-03-06 13:01:30 +0500

    phonebook backend step5; POST request implemented

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,11 +6,20 @@ const app = express()
 app.use(express.json())
 const http = require('http')
 
+const getRandomInt = (max) => {
+	return (Math.floor(Math.random() * max))
+}
+
 const read_parseJson = (filepath) => {
 	const rawdata = fs.readFileSync(path.resolve(__dirname, filepath));
 	const persons = JSON.parse(rawdata);
 	console.log(persons);
 	return persons
+}
+
+const parse_writeJson = (filepath, newJson) => {
+	const json = JSON.stringify(newJson)
+        const write = fs.writeFileSync(filepath, json)
 }
 
 const get_person = (id) => {
@@ -34,7 +43,7 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-	parsed_db = read_parseJson('db.json')
+	const parsed_db = read_parseJson('db.json')
 	response.send(`
 		<p>Phonebook has info for ${parsed_db.length} people</p>
 		<p>${new Date().toISOString()}</p>`)
@@ -44,10 +53,19 @@ app.delete('/api/persons/delete/:id', (request, response) => {
 	const id = Number(request.params.id)
 	const db = read_parseJson('db.json')
 	const updated_db = db.filter(person => person.id !== id)
-	const json = JSON.stringify(updated_db)
-        const write = fs.writeFileSync('db.json', json)
+	parse_writeJson('db.json', updated_db)
 	console.log('deletion request on', id, 'done', 'object written', json)
 	console.log(write)
+})
+
+app.post('/api/persons', (request, response) => {
+	const newId = getRandomInt(1000)
+	const parsed_db = read_parseJson('db.json')
+	const reqInfo = request.body
+	parsed_db.push({id: newId, name: reqInfo.name, number: String(reqInfo.number)})
+	parse_writeJson('db.json', parsed_db)
+	console.log(parsed_db)
+	//parse_writeJson('db.json', parsed_db)
 })
 
 const PORT = 3001
