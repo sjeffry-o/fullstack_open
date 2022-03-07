8ed475489e13570f6e0420f3f38aaa91f4aaf9c5
sjeffry-o <kronizzyd@gmail.com>
2022-03-07 11:03:40 +0500

    MongoDB database cmd-line interaction interface using mongoose

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length !== 3 && process.argv.length !== 5) {
+  console.log('Provide the password, name and number as an argument for addition: node mongo.js <password> <name> <number> \n or just password for showing database: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.3yz2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length > 3)
+{
+	const newName = process.argv[3]
+	const newNumber = process.argv[4]
+	const person = new Person({
+	  name: newName,
+	  number: newNumber,
+	})
+	person
+	.save()
+	.then(result => {
+	  console.log(`added ${newName} number ${newNumber} to the phonebook`)
+	  mongoose.connection.close()
+	})
+}
+else
+{
+	Person
+	  .find({})
+	  .then(persons=> {
+	    console.log('phonebook:\n', persons)
+	    mongoose.connection.close()
+	  })
+}
