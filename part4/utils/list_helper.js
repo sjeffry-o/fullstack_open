10f89e87a18664a7f93fc3eb95e1157eef1c49a8
sjeffry-o <kronizzyd@gmail.com>
2022-03-11 09:28:23 +0500

    totalLikes function done 'n tested properly

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likeCounter = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogs.length === 0
+  ? 0
+  : blogs.reduce(likeCounter, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
