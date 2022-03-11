b79495d7faeb707ae716e657ef5da6fa1070138c
sjeffry-o <kronizzyd@gmail.com>
2022-03-11 11:21:56 +0500

    favoriteBlog finder w/ tests written

--- part4/tests/favBlog.test.js
+++ part4/tests/favBlog.test.js
@@ -3,8 +3,8 @@ const listHelper = require('../utils/list_helper')
 describe('total likes', () => {
   test('zero blogs, equals zero total likes', () => {
     const ZeroBlogs = []
-    const countLikes = listHelper.totalLikes(ZeroBlogs)
-    expect(countLikes).toBe(0)
+    const countLikes = listHelper.favoriteBlog(ZeroBlogs)
+    expect(countLikes).toEqual({})
   })
 
   const listWithOneBlog = [
@@ -19,8 +19,17 @@ describe('total likes', () => {
   ]
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+    )
   })
 
   const listWithManyBlogs = [
@@ -74,8 +83,17 @@ describe('total likes', () => {
     }  
   ]
   test('when list has many blogs, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithManyBlogs)
-    expect(result).toBe(36)
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual(
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+    )
   })
 })
 
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -11,6 +11,17 @@ const totalLikes = (blogs) => {
   : blogs.reduce(likeCounter, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let maxLikesIdx = 0
+  blogs.forEach((blog, i, blogs) => {
+    if (blogs[i].likes > blogs[maxLikesIdx].likes)
+      maxLikesIdx = i
+  })
+  return blogs.length === 0
+  ? {}
+  : blogs[maxLikesIdx]
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
