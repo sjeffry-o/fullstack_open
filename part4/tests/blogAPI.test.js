f66231ebd639107a9f41fde4eff6727e5576988f
sjeffry-o <kronizzyd@gmail.com>
2022-03-15 17:22:47 +0500

    requested JSON have an 'id' param test, draft for POST request test

--- part4/tests/blogAPI.test.js
+++ part4/tests/blogAPI.test.js
@@ -4,12 +4,40 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-}, 10000)
+}, 5000)
+
+test("blogs have 'id' parameter", async () => {
+  await api
+    .get('/api/blogs')
+    .then(response => {
+      //console.log(response._body)
+      expect(response._body[0].id).toBeDefined()
+    })
+}, 5000)
+
+/*test("POST request to '/api/blogs' is succesful", async () => {
+  await api
+    .post('/api/blogs')
+    .send(
+      {
+        "title": "hey",
+        "author": "Bradbury",
+        "url": "definedeeee.com",
+        "likes": 2
+      }
+    )
+    .then(response => {
+      console.log(response)
+      //expect(response._body[response._body.length - 1]).toEqual(newBlog)
+    })
+    .expect(201)
+    
+}, 10000)*/
 
 afterAll(() => {
   mongoose.connection.close()
