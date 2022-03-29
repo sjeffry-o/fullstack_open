d88a70a6fdc29e395ebee2ce2d532ec75b00f924
sjeffry-o <kronizzyd@gmail.com>
2022-03-29 15:08:45 +0500

    blogsRouter and its methods implemented, not tested

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,78 @@
+const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const auth = request.get('authorization')
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    return auth.substring(7)
+  }
+  return null
+}
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+              .find({})
+              .populate('user', {username:1, name:1})
+
+  response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog)
+    response.json(blog.toJSON())
+  else
+    response.status(404).end()
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const { title, author, url } = request.body
+
+  const token =  getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Note({
+    title,
+    author,
+    url,
+    date: new Date(),
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url } = request.body
+
+  const blog = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
