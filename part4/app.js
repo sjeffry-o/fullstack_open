ced4246147badad0f433346d9c9675b573237b0d
sjeffry-o <kronizzyd@gmail.com>
2022-03-30 14:50:32 +0500

    middleware logging, blogsRouter connected; problems with authorization

--- part4/app.js
+++ part4/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+
 const mongoose = require('mongoose')
-const logger = require('./utils/logger')
-const config = require('./utils/config')
 const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
 const loginRouter = require('./controllers/login')
+const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 
 const mongoUrl = config.MONGODB_URL
@@ -18,25 +22,13 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
