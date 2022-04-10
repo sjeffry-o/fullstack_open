0126c39167ae9e104e62d5802953ca46c2be29e7
sjeffry-o <kronizzyd@gmail.com>
2022-04-10 15:40:33 +0500

    cypress E2E testing init; show log in form test

--- part4/app.js
+++ part4/app.js
@@ -11,6 +11,7 @@ const middleware = require('./utils/middleware')
 const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const testingRouter = require('./controllers/testing')
 
 const mongoUrl = config.MONGODB_URL
 mongoose.connect(mongoUrl)
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/testing', testingRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
